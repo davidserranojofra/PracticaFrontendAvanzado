f62a4f7913e8b92a1cc3ed8b7ee252844086191b
David Serrano Jofra <davidserranojofra@gmail.com>
2017-08-04 19:39:37 +0200

    finalizo con un smooth scroll la pestaña ir arriba acabada 100%

--- src/js/flechaArriba.js
+++ src/js/flechaArriba.js
@@ -1,18 +1,28 @@
 const $ = require("jquery");
 
+//miro cuando se hace scroll en la pantalla
 $(window).bind('scroll', function () {
     let anchuraDeVentana = $(window).width();
-    console.log(anchuraDeVentana);
     let num;
+
+    //compruebo anchura de ventana para que se muestre justo perder el título
     if (anchuraDeVentana <= 1023) {
         num = 70;
     } else {
         num = 140;
     }
-
+    //muestro/oculto pestaña ir arriba
     if ($(window).scrollTop() > num) {
 		$('#flecha').addClass('desplegar-flecha');
 	} else {
 		$('#flecha').removeClass('desplegar-flecha');
 	}
+});
+
+//Animacion Smooth Scroll para ir arriba
+$('#flecha').on("click", function() {
+    let irArriba = $(this).attr('href');
+
+    $('body, html').animate({scrollTop: $(irArriba).offset().top - 47}, 450);
+      console.log(irArriba);
 });
